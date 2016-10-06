f6c9dd44863bd201e3bad2a4a9bf51b4d4199198
Stijn <stijn.deridder.1@student.kdg.be>
2016-10-06 15:25:52 +0200

    Opdracht-Comments

---
+++ Opdracht-comments.php
@@ -0,0 +1,23 @@
+<?php
+/*
+	Stijn De Ridder
+	stijn.deridder.1@student.kdg.be
+*/
+	$naam = 'De Ridder';
+	$voornaam = 'Stijn';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Comments</title>
+</head>
+
+<body>
+	<p><?php echo $naam ; ?></p>
+	<p><?php echo $voornaam ; ?></p>
+
+</body>
+</html>
