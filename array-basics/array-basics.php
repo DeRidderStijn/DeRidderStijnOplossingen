816ff01d36987c76ac3f71c0e4270b51d6a051b8
Stijn <stijn.deridder.1@student.kdg.be>
2016-10-12 20:29:36 +0200

    array basis

---
+++ array-basics/array-basics.php
@@ -0,0 +1,27 @@
+<?php
+	$dieren1 = array('eend','schaap','hond','kat','slang','ezel','geit','aap','flamingo','dolfijn');
+	$dieren2 = ['eend','schaap','hond','kat','slang','ezel','geit','aap','flamingo','dolfijn'];
+
+	$voertuigen = array('landvoertuigen' => array(
+			'auto', 'motor', 'fiets'),
+		'watervoertuigen' => array(
+			'boot'),
+		'luchtvoertuigen' => array(
+			'vliegtuig'));
+
+	var_dump($dieren1);
+	var_dump($voertuigen);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Comments</title>
+</head>
+
+<body>
+
+</body>
+</html>
