5ab326519abb6ae8006bbae08c89d1387dd672e0
Stijn <stijn.deridder.1@student.kdg.be>
2016-11-24 12:27:48 +0100

    Classes-Portfolio

---
+++ classesPortfolio/html/index-body.partial.php
@@ -0,0 +1,32 @@
+	<section class="clearfix"> 
+		<div class="main-content">
+			<div class="intro">
+			<h3>Over mezelf</h3>
+			
+				<p>
+					Hallo, ik ben Stijn De Ridder. Ik studeer Multimedia Technology (afstudeerrichting Web & UX) aan 
+				   	de Karel de Grote hogeschool te Hoboken.<br>
+				   	Ik ben 23 jaar en werk op vrije dagen als software en interface developper bij Van Bouwel Kranen.
+				</p>
+			</div>
+			<div class="content">
+				<p>
+					In mijn vrije tijd ben ik veel bezig met muziek beluisteren en optredens bekijken.
+					Zo bezoek ik jaarlijks best veel concertjes en indien mogelijk nog een/enkele festivals.
+				</p>	
+				<p>
+					Sinds kort ben ik ook bezig met mezelf Spaans aan te leren. Dit gaat in combinatie met school en werk (en in zelfsstudie)
+					misschien niet zo vlot, maar ik ben nog steeds gemotiveerd.
+				</p>
+			</div>
+			
+
+		</div>
+		<div class="side-panel">
+			<div class="content">
+				<p>
+					Als u nog vragen heeft kan u mij altijd contacteren op <a href="mailto:stijn.deridder.1@student.kdg.be?Subject=InfoPortfolio" target="_top">stijn.deridder.1@student.kdg.be</a>
+				</p>
+			</div>
+		</div>
+	</section>	
